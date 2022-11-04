2fb1e3cf5814b0f9149babe6ad4c40f86dee2b49
Alejandro Salas <asalas.wd@outlook.com>
2022-11-03 20:52:45 -0400

    Add method to add int to BitSet

--- src/com/java21days/HolidaySchedule.java
+++ src/com/java21days/HolidaySchedule.java
@@ -7,5 +7,14 @@ public class HolidaySchedule {
 
     public HolidaySchedule(){
         sked = new BitSet(365);
+        int[] holiday = { 1, 15, 50, 148, 185, 246, 281, 316, 326, 359};
+        for(int i=0; i<holiday.length; i++){
+            System.out.println("Adding " + holiday[i]);
+            addHoliday(holiday[i]);
+        }
+    }
+
+    public void addHoliday(int dayToAdd){
+        sked.set(dayToAdd);
     }
 }
